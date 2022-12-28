8d9b7c96e41576ef975772a1197cd4e1f4b346d6
Sabari <sabarihiswaran555@gmail.com>
2022-12-28 11:45:49 +0530

    Day1-challenge-completed

--- Day1/drumkit.js
+++ Day1/drumkit.js
@@ -1,1 +1,33 @@
-console.log("Hello JS")
+
+const playsound =(e) => {
+
+    const findAudio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
+
+    const findDiv = document.querySelector(`.Kit[data-key = "${e.keyCode}"]`);
+
+    if(!findAudio){
+        return;
+    }
+
+    findAudio.currentTime = 0;
+
+    findAudio.play();
+
+    findDiv.classList.add("playing")
+
+    const kits = document.querySelectorAll('.Kit')
+
+    function removingClass(e) {
+        if(e.propertyName != "transform"){
+             return;
+        }
+       
+        this.classList.remove("playing")
+    }
+
+    kits.forEach(items => items.addEventListener('transitionend', removingClass))
+
+  
+}
+
+window.addEventListener("keydown", playsound)
