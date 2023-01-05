f9baf90a7f6863e7ab65268f1af1f82d8d0dd750
Sabari <sabarihiswaran555@gmail.com>
2023-01-05 18:08:29 +0530

    Day8-Challenge-Completed

--- Day8/canvas.js
+++ Day8/canvas.js
@@ -1,1 +1,45 @@
-console.log("Hello js")
+window.addEventListener("load", () => {
+    const canvas = document.querySelector("#drawarea")
+
+    const ctx = canvas.getContext("2d")
+
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+
+    let isDrawing = false;
+
+    canvas.addEventListener("mousedown", (e) => {
+        isDrawing = true
+        draw(e)
+    })
+
+    canvas.addEventListener("mouseup", () => {
+        isDrawing = false
+        ctx.beginPath()
+    })
+
+    canvas.addEventListener("mousemove", draw)
+
+    let hue = 0;
+
+    function draw(e) {
+
+        if (!isDrawing) return;
+
+        ctx.lineWidth = 100
+        ctx.lineCap = "round"
+        ctx.strokeStyle = `hsl(${hue} , 100% ,50%)`
+
+        ctx.beginPath()
+        ctx.lineTo(e.clientX, e.clientY)
+        ctx.moveTo(e.clientX, e.clientY)
+        ctx.stroke()
+
+        hue++
+        if (hue >= 360) {
+            hue = 0
+        }
+
+    }
+
+})
