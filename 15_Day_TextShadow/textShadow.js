20b02005de8d8fa67bbf57ae21c2d7a3c54b6513
Sabari <sabarihiswaran555@gmail.com>
2023-01-12 13:19:41 +0530

    Day15-challenge-completed

--- 15_Day_TextShadow/textShadow.js
+++ 15_Day_TextShadow/textShadow.js
@@ -1,23 +1,27 @@
 
-const textdiv = document.querySelector(".textdiv")
+const textdiv = document.querySelector(".textdiv") //whole div
 
-const content = textdiv.querySelector("h1")
+const content = textdiv.querySelector("h1") //h1 content
 
-textdiv.addEventListener("mousemove" , handleMove)
+textdiv.addEventListener("mousemove" , handleMove) // addding event listener to whole div
 
 function handleMove(e){
-    let {offsetX:x , offsetY:y} = e
-    const {offsetWidth:width , offsetHeight:height} = textdiv
+    let {offsetX:x , offsetY:y} = e       // fetching where the mouse is currently present
+    const {offsetWidth:width , offsetHeight:height} = textdiv   //Total width and height of textdiv
 
-    if(this !== e.target){
+    if(this !== e.target){   
+        //this is whole div and when cursor touches h1 content then it is e.target
        
         x = x +e.target.offsetLeft
         y = y+e.target.offsetTop 
+
+        //adding left and top length to h1 content
     }
 
     const walk = 100; // 100px
     
-    const xwalk = x / width * walk - (walk/2)
+    //To determine the horizantal and vertical px of text shadow
+    const xwalk = x / width * walk - (walk/2)   
 
     const ywalk = y / height * walk - (walk/2)
 
