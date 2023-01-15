11f3a8f30ae33d604e4b4242404187eb6c6e5dab
Sabari <sabarihiswaran555@gmail.com>
2023-01-15 20:49:48 +0530

    Day17-challenge-completed

--- 17_Day_LocalStorage/todolist.js
+++ 17_Day_LocalStorage/todolist.js
@@ -1,1 +1,62 @@
-console.log("hello")
+
+const todolist = document.querySelector(".todolist")
+
+const form = document.querySelector(".add-items")
+
+form.addEventListener("submit" , captureInput)
+
+const ListData = JSON.parse(localStorage.getItem("items")) || []
+
+function captureInput(e){
+
+    e.preventDefault()
+
+    const text  = this.querySelector("[name = listname]").value
+   
+    
+    const data = {
+        text ,    //which is similar to text : text
+        data : false
+    }
+
+    ListData.push(data)
+
+    populateList(ListData , todolist)
+
+    localStorage.setItem("items" , JSON.stringify(ListData))
+
+    
+    this.reset()
+}
+
+function populateList( listOfData = [] , todolist ){
+
+   todolist.innerHTML =  listOfData.map((item,i) => {
+        
+        return `
+            <li> 
+            <input type="checkbox" data-index=${i} id="item${i}" ${item.data ? "checked" : "" } />
+            <label for="item${i}">  ${item.text} </label>
+            </li>
+        `
+    }).join('')
+
+}
+
+function toggleCheck(e){
+   if(!e.target.matches("input")) return null
+
+    const inputBox  =e.target
+    const indexOfBox = inputBox.dataset.index
+    
+    ListData[indexOfBox].data = !ListData[indexOfBox].data
+    localStorage.setItem("items" , JSON.stringify(ListData))
+
+    populateList(ListData , todolist)
+}
+
+
+todolist.addEventListener("click" , toggleCheck)
+
+
+populateList(ListData , todolist)
