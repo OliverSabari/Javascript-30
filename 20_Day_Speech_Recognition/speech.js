d1aa7b5d251a2453fb6216e65e6fc7d84509b690
Sabari <sabarihiswaran555@gmail.com>
2023-01-24 10:32:56 +0530

    Day20 completed

--- 20_Day_Speech_Recognition/speech.js
+++ 20_Day_Speech_Recognition/speech.js
@@ -1,1 +1,30 @@
-console.log("hello")
+
+window.speechRecognition = window.speechRecognition || window.webkitSpeechRecognition
+
+const recognition = new speechRecognition()
+
+recognition.interimResults = true
+
+const speechdiv = document.querySelector(".speech-div")
+
+let p = document.createElement("p")
+
+speechdiv.appendChild(p)
+
+recognition.addEventListener("result", (e) => {
+    const transcript = Array.from(e.results)
+        .map(result => result[0])
+        .map(result => result.transcript)
+        .join("")
+    p.textContent = transcript
+
+    if (e.results[0].isFinal) {
+        p = document.createElement("p")
+        speechdiv.appendChild(p)
+    }
+}
+)
+
+recognition.addEventListener("end", recognition.start)
+
+recognition.start()
